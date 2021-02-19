69928cc0a876719969c3ed47ffd84a05bc90aec7
Azeem Mumtaz <azeemigi@gmail.com>
2021-02-19 10:43:57 +0530

    Solution for Day 4 (part 2)

--- day4/app.js
+++ day4/app.js
@@ -11,6 +11,10 @@ let parsePassport = x => {
     return passport;
 };
 
+let checkRequiredData = passport => {
+    return passport['byr'] && passport['iyr'] && passport['eyr'] && passport['hgt'] && passport['hcl'] && passport['ecl'] && passport['pid'];
+};
+
 let passports = readFileSync('input-files/day4.txt')
                 .toString('utf-8')
                 .split('\n\n')
@@ -21,7 +25,7 @@ let validPassportCount = 0;
 
 // Part1
 passports.forEach(passport => {
-    if (passport['byr'] && passport['iyr'] && passport['eyr'] && passport['hgt'] && passport['hcl'] && passport['ecl'] && passport['pid']) {
+    if (checkRequiredData(passport)) {
         validPassportCount++;
     }
 });
@@ -29,6 +33,7 @@ passports.forEach(passport => {
 console.log(`Part1 => Valid Passports: ${validPassportCount}`)
 
 let validate = (k, v) => {
+    let hgtValue;
     switch (k) {
         case 'byr':
             return v >= 1920 && v <= 2002;
@@ -37,8 +42,42 @@ let validate = (k, v) => {
         case 'eyr':
             return v >= 2020 && v <= 2030;
         case 'hgt':
-            
+            if (hgtValue = /^([\d.]+)cm$/.exec(v)) {
+                return hgtValue[1] >= 150 && hgtValue[1] <= 193;
+            };
+            if (hgtValue = /^([\d.]+)in$/.exec(v)) {
+                return hgtValue[1] >= 59 && hgtValue[1] <= 76;
+            };
+            return false;
+        case 'hcl':
+            return /^#[0-9a-f]{6}$/.test(v);
+        case 'ecl':
+            return /(amb|blu|brn|gry|grn|hzl|oth)/.test(v);
+        case 'pid':
+            return /^[0-9]{9}$/.test(v);
+        case 'cid':
+            return true;
         default:
-            break;
+            return false;
     }
 };
+
+let validatePassport = passport => {
+    for (const [key, value] of Object.entries(passport)) {
+        if(validate(key, value) === false) {
+            return false;
+        }
+    }
+    return true;
+};
+
+let validPassportCountPart2 = 0;
+
+passports.forEach(passport => {
+    if (checkRequiredData(passport) && validatePassport(passport)) {
+        validPassportCountPart2++;
+    }
+});
+
+
+console.log(`Part2 => Valid Passports: ${validPassportCountPart2}`)
